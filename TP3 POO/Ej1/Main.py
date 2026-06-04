b0a543d3166bb00b89e97199d59bb308dc4235e0
IaraUriarte <iaraherlein0001@gmail.com>
2026-06-03 21:31:52 -0300

    Modificaciones en EJ1

--- TP3 POO/Ej1/Main.py
+++ TP3 POO/Ej1/Main.py
@@ -5,7 +5,7 @@ def main():
 	Num1 = Entero(6)
 	Num2 = Entero(4)
 
-	print('--- Resultado: Clase Entero ---')
+	print('Resultado: clase Entero')
 	print(f'Número inicial (Num1): {Num1.get_numero()}')
 
 	Num1.set_numero(5)
@@ -15,7 +15,7 @@ def main():
 	print(f'¿{Num1.get_numero()} es par? {'Sí' if Num1.esPar() else 'No'}')
 	print(f'¿{Num1.get_numero()} es impar? {'Sí' if Num1.esImpar() else 'No'}')
 
-	print('\n--- Operaciones sobre Num2 ---')
+	print('\nOperaciones sobre Num2')
 	print(f'Número (Num2): {Num2.get_numero()}')
 	print(f'Factorial de {Num2.get_numero()}: {Num2.factorial()}')
 	print(f'¿{Num2.get_numero()} es primo? {'Sí' if Num2.esPrimo() else 'No'}')
